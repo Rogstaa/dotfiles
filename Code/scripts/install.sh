48857984440d07a31b0f925caa47291a0f738a3c
Hoegbo <hoegbo@protonmail.com>
2023-01-15 23:38:50 +0100

    initial

---
+++ Code/scripts/install.sh
@@ -0,0 +1,10 @@
+sudo xbps-install xorg libXinerama-devel libX11-devel libXft-devel hsetroot xsetroot xclip pulseaudio pavucontrol neovim base-devel git curl vulkan-loader mesa-vulkan-radeon amdvlk mesa-vaapi mesa-vdpau flatpak qutebroser g810-led xtools python3-adblock
+
+
+#add flathub remote
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+echo add new github remote
+
+#add new ssh key 
+ssh-keygen -t ed25519 -C "hoegbo@protonmail.com"
